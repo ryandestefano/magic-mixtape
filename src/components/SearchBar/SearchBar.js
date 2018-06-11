9ef47dc4e577d1f86e87f6adcaf6aab34b382e7d
ryandestefano <ryandestefano@househappy.com>
2018-06-11 14:31:10 -0700

    Added functionality to search button and made search occur when enter key is pressed

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -4,24 +4,26 @@ class SearchBar extends React.Component {
   constructor(props) {
     super(props);
     this.search = this.search.bind(this);
-    this.handleSearchTermChange = this.handleSearchTermChange.bind(this);
+    this.foo = this.foo.bind(this);
   }
 
-  search(searchTerm) {
-    this.props.onSearch(searchTerm);
+  search() {
+    this.props.onSearch(document.getElementById('search-bar').value);
   }
 
-  handleSearchTermChange(e) {
-    this.search(e.target.value);
+  foo(e) {
+    if (e.which === 13) {
+      this.search();
+    }
   }
 
   render() {
     return (
       <div className={this.props.numberOfSearchResults > 0 ? "SearchBar" : "SearchBar no-search-results"}>
         <div className="input-container">
-          <input onChange={this.handleSearchTermChange} placeholder="Enter A Song Title" />
+          <input id="search-bar" onKeyPress={this.foo} placeholder="Enter A Song Title" />
         </div>
-        <a>Search</a>
+        <a onClick={this.search}>Search</a>
       </div>
     );
   }
