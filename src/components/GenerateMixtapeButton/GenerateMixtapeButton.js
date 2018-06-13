297c27fc33d4d2cb43ca5818de642ba881a15718
ryandestefano <ryandestefano@househappy.com>
2018-06-12 21:39:57 -0700

    Updates

--- src/components/GenerateMixtapeButton/GenerateMixtapeButton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class GenerateMixtapeButton extends React.Component {
-  render() {
-    return <a className="get-mixtape" onClick={this.props.getRecommendations}>Get mixtape</a>;
-  }
-}
-
-export default GenerateMixtapeButton;
